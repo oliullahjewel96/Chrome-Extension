8f806815d48cb51a3985fc343fbec208d37acff6
Oli Ullah Jewel <oli.jewel@northsouth.edu>
2022-01-06 21:34:19 +0600

    Rendered lists from localStorage

--- app.js
+++ app.js
@@ -1,20 +1,42 @@
+let myLeads = [];
+
 const saveBtn = document.getElementById("input-btn");
 const inputEl = document.getElementById("input-el");
 const ulEl = document.querySelector("#ul-el");
+let error = document.querySelector("#error");
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 
-let myLeads = ["google", "amazon", "tesla", "sillicon valley"];
-
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage;
+    renderLists();
+}
 saveBtn.addEventListener("click", toSaveLinks);
 
 function toSaveLinks() {
     myLeads.push(inputEl.value);
-    console.log(myLeads);
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    renderLists();
+
+    inputEl.value = "";
 }
 
-for (item = 0; item < myLeads.length; item++) {
-    // ulEl.innerHTML += "<li>" + myLeads[item] + "</li>";
+function renderLists() {
+    let listItems = "";
+    for (item = 0; item < myLeads.length; item++) {
+        // ulEl.innerHTML += "<li>" + myLeads[item] + "</li>";
 
-    const li = document.createElement("li");
-    li.textContent = myLeads[item];
-    ulEl.append(li);
+        // const li = document.createElement("li");
+        // li.textContent = myLeads[item];
+        // ulEl.append(li);
+
+        listItems += `
+        <li>
+             <a href="${myLeads[item]}" target="_blank">
+             ${myLeads[item]}
+             </a>
+             
+        </li>`;
+    }
+
+    ulEl.innerHTML = listItems;
 }
